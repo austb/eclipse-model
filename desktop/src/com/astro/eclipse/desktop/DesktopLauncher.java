243e9e7315816864ff89b9bc85f294a98924e2a7
Austin Blatt <austin.blatt@puppetlabs.com>
2015-10-20 15:21:29 -0700

    Initial commit. LibGDX project.

---
+++ desktop/src/com/astro/eclipse/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.astro.eclipse.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.astro.eclipse.EclipseModel;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.samples = 2;
+		new LwjglApplication(new EclipseModel(), config);
+	}
+}
